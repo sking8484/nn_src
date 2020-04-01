d376ea1ce23db392b8b1eba64e3cd89bd55e7eae
Darst King <sking8484@gmail.com>
2020-03-31 18:09:14 -0700

    First Additions To setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,6 @@
+from setuptools import setup
 
+
+setup(
+    name = "QCNN",version="0.0.1",description="Deep Neural Network module for classification/regression",py_modules=['NeuralNetwork'],package_dir = {'':'src'}
+)
