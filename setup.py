7947a16c708df0c87f8a725c05b0f40ce24da9d3
Darst King <sking8484@gmail.com>
2020-06-04 17:12:54 -0700

    Changed to version 1.0.3. Rebuilt wheels to be compatible with catalina

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open('README.md', 'r') as file:
 
 
 setup(
-    name = "QCNN",version="1.0.1",description="Deep Neural Network module for classification/regression",py_modules=['QCNN'],package_dir = {'':'src'},classifiers = [
+    name = "QCNN",version="1.0.3",description="Deep Neural Network module for classification/regression",py_modules=['QCNN'],package_dir = {'':'src'},classifiers = [
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'License :: OSI Approved :: MIT License'],
     long_description = long_description,
     long_description_content_type='text/markdown',
-    install_requires = ['matplotlib~=2.2.2',
-    'pandas~=0.22.0',
-    'numpy~=1.18.2']
+    install_requires = ['matplotlib>=2.2.2',
+    'pandas>=0.22.0',
+    'numpy>=1.18.2']
 
 
 )
